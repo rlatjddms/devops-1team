e532f960bb36954a63fe45383fb7116e40fdb7c4
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-08 21:25:32 +0900

    [chore] #15 Inventory 엔티티 컬럼 nullable = false 설정

--- backend/src/main/java/com/spicy/backend/inventory/domain/Inventory.java
+++ backend/src/main/java/com/spicy/backend/inventory/domain/Inventory.java
@@ -1,20 +1,19 @@
 package com.spicy.backend.inventory.domain;
 
 import com.spicy.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
-@Table
 @Getter
 @NoArgsConstructor
 public class Inventory extends BaseEntity {
@@ -23,16 +22,20 @@ public class Inventory extends BaseEntity {
     private Long id;
 
     //상품ID
+    @Column(nullable = false)
     private Long productId;
 
     //수량
+    @Column(nullable = false)
     private int quantity;
 
     //유통기한
+    @Column(nullable = false)
     private LocalDate expirationDate;
 
     //상품상태
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private LotStatus status;
 
 }
