d82d685b500b2e0cfd2c2588307632074ad4dc49
rlatjddms <kseo_o@naver.com>
2026-01-08 19:32:17 +0900

    [feat] #18 User 엔티티 구현

---
+++ backend/src/main/java/com/spicy/backend/user/domain/User.java
@@ -0,0 +1,39 @@
+package com.spicy.backend.user.domain;
+
+import com.spicy.backend.global.entity.BaseEntity;
+import com.spicy.backend.user.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+}
