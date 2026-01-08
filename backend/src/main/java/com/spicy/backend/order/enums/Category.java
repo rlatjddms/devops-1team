6475ceb3e13b87766d6e1abeab1d19ef5535c71c
chaewookim <chaewookim2877@gmail.com>
2026-01-08 15:41:39 +0900

    [feat] #12 Category enum 구현

--- backend/src/main/java/com/spicy/backend/order/enums/Category.java
+++ backend/src/main/java/com/spicy/backend/order/enums/Category.java
@@ -1,4 +1,25 @@
 package com.spicy.backend.order.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Category {
+    // 신선 식품
+    FRESH_FOOD("신선 식품"),
+
+    // 가공 식품
+    PROCESSED_FOOD(""),
+
+    // 냉동 식품
+    FROZEN_FOOD(""),
+
+    // 식기/용기
+    GOODS("비품/용기"),
+
+    // 음료
+    BEVERAGE("음료");
+
+    private final String description;
 }
