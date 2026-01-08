864c4d10d986d8543fc27127208d8184be0ef86c
chaewookim <chaewookim2877@gmail.com>
2026-01-08 16:28:50 +0900

    [feat] #12 CartItem 엔티티 구현

--- backend/src/main/java/com/spicy/backend/order/domain/CartItem.java
+++ backend/src/main/java/com/spicy/backend/order/domain/CartItem.java
@@ -1,9 +1,11 @@
 package com.spicy.backend.order.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,5 +23,15 @@ public class CartItem {
     private Long id;
 
     // fk: 가맹점 식별 번호
+    @Column(nullable = false)
+    private Long storeId;
 
+    // fk: 상품 식별 번호
+    @Column(nullable = false)
+    private Long productId;
+
+    // 수량
+    @Column(nullable = false)
+    @Size(min = 1)
+    private Long quantity;
 }
