32214c6b639b9226d183af26215738d17f8b17cd
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 16:40:27 +0900

    [feat] #20 Request 작성

---
+++ backend/src/main/java/com/spicy/backend/inventory/dto/request/InventoryRequest.java
@@ -0,0 +1,13 @@
+package com.spicy.backend.inventory.dto.request;
+
+import java.time.LocalDate;
+
+public record InventoryRequest(
+        // 상품ID
+        Long productId,
+        // 샹품 수량
+        int quantity,
+        // 유통기한
+        LocalDate expirationDate
+) {
+}
