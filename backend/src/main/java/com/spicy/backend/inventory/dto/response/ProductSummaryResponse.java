bb940e507f41ca02d727c6d470f9795c80923961
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 17:43:51 +0900

    [feat]  #20 inventory api document (#30)
    * [feat] #20 InventoryController 작성

---
+++ backend/src/main/java/com/spicy/backend/inventory/dto/response/ProductSummaryResponse.java
@@ -0,0 +1,18 @@
+package com.spicy.backend.inventory.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ProductSummaryResponse(
+        // 상품 이름
+        String productName,
+        // 상품 가격
+        BigDecimal price,
+        // 최소 수량
+        int minimumQuantity,
+        // 총 수량
+        int totalQuantity,
+        //총 상품
+        List<InventoryLotResponse> products
+) {
+}
