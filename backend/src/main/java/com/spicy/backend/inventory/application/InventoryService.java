f9b918d2df4c7c0c169c5e5b0f26e764cbd08237
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 16:41:54 +0900

    [chore] #20 Service 생성

---
+++ backend/src/main/java/com/spicy/backend/inventory/application/InventoryService.java
@@ -0,0 +1,10 @@
+package com.spicy.backend.inventory.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+}
