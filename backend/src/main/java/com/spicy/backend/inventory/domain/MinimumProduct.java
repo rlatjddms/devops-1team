d0482bc9976a2064f0783a9932cf942464e5e9c3
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 11:12:04 +0900

    [feat] #15 어노테이션 추가

--- backend/src/main/java/com/spicy/backend/inventory/domain/MinimumProduct.java
+++ backend/src/main/java/com/spicy/backend/inventory/domain/MinimumProduct.java
@@ -6,12 +6,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Entity
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MinimumProduct extends BaseEntity {
 
     @Id
