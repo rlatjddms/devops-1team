7015ed9ad7c64f80fe477c86a6391a1c6a8a4f8c
chaewookim <chaewookim2877@gmail.com>
2026-01-09 15:07:26 +0900

    [feat] #14 주문번호 생성기 구현

---
+++ backend/src/main/java/com/spicy/backend/order/dto/request/OrderItemRequest.java
@@ -0,0 +1,24 @@
+package com.spicy.backend.order.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record OrderItemRequest(
+        // 상품 이름
+        @NotBlank
+        String productName,
+
+        // 주문 수량
+        @NotNull
+        @Min(1)
+        Integer quantity,
+
+        // 개별 단가
+        @NotNull
+        @Min(1)
+        BigDecimal unitPrice
+) {
+}
