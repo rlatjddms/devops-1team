bc3c0fbc50c7b5844c601a16f0e71f38f2241a0f
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 17:05:01 +0900

    [fix] #20 오타수정

--- backend/src/main/java/com/spicy/backend/inventory/dto/request/InventoryRequest.java
+++ backend/src/main/java/com/spicy/backend/inventory/dto/request/InventoryRequest.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public record InventoryRequest(
         // 상품ID
         Long productId,
-        // 샹품 수량
+        // 상품 수량
         int quantity,
         // 유통기한
         LocalDate expirationDate
