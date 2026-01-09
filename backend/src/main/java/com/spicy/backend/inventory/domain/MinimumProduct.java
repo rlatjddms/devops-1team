d8e83aca23f47e8e2d5bd441277cb835146c1241
cho-yunho01 <04whdbsgh30@naver.com>
2026-01-09 10:41:22 +0900

    [refactor] #15 클래스 명 수정

--- backend/src/main/java/com/spicy/backend/inventory/domain/MinimumProduct.java
+++ backend/src/main/java/com/spicy/backend/inventory/domain/MinimumProduct.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class ProductMinimum extends BaseEntity {
+public class MinimumProduct extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
