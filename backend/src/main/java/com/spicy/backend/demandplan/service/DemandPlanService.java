3c62d05b1410559bbf880d6c8fa5e998d7512872
yoocy0 <yoocy0328@gmail.com>
2026-01-10 11:11:47 +0900

    [chore] #21 서비ìprocess 메서드 반환값 변경 및 TODO 작성

--- backend/src/main/java/com/spicy/backend/demandplan/service/DemandPlanService.java
+++ backend/src/main/java/com/spicy/backend/demandplan/service/DemandPlanService.java
@@ -13,18 +13,18 @@ public class DemandPlanService {
     private final ExternalDataFetcher infoProvider;
     private static final double SAFETY_STOCK_RATE = 1.2;
 
-    public void process(Long productId) {
+    public String process(Long productId) {
         // 재고 및 유효 값 조회
         StockResponseDto stock = infoProvider.getStock(productId);
 
-        //
+        // 유효성 판단 로직
         if(stock.totalQuantity() < stock.minimumQuantity()) {
+            // 추가 주문 권장 수량 메시지 출력
             String recommendationMessage = createRecommendationMessage(productId);
-            System.out.println("현재 재고는 " + stock.totalQuantity() +
-                    "개 입니다. 추가 주문이 필요합니다." + recommendationMessage);
-        } else {
-            System.out.println("현재 재고가 충분합니다.");
+            return "현재 재고는 " + stock.totalQuantity() +
+                    "개 입니다. 추가 주문이 필요합니다." + recommendationMessage;
         }
+        return null;
     }
 
     private String createRecommendationMessage(Long productId) {
@@ -34,6 +34,8 @@ public class DemandPlanService {
         int totalSales = sales
                 .stream()
                 .mapToInt(i -> i).sum();
+
+        // TODO: 최근 7일 간 재고 주문 수량이 0건인 경우 로직 추가
 
         // 최근 7일의 총 매출과 안전 수량 배율 상수를 곱연산하여 계산(배율 조정 가능)
         int recommended = (int)(totalSales * SAFETY_STOCK_RATE);
