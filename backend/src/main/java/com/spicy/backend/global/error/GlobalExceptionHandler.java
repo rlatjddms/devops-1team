8d50c939149825c011d8dddd1b6bec7fb8ae1d64
rlatjddms <kseo_o@naver.com>
2026-01-12 16:22:27 +0900

    [feat] #35 Validation 예외 전역 처리 추가

--- backend/src/main/java/com/spicy/backend/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/spicy/backend/global/error/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package com.spicy.backend.global.error;
 import com.spicy.backend.global.common.ApiResponse;
 import com.spicy.backend.global.error.errorcode.GlobalErrorCode;
 import com.spicy.backend.global.error.exception.BusinessException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,5 +31,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ApiResponse.error(GlobalErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+
+        String combinedMessage = e.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(combinedMessage));
     }
 }
