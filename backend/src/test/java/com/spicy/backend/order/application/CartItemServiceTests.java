bf1c696aeef91e52ac08b4de9f9dd9ca45ba6937
chaewookim <chaewookim2877@gmail.com>
2026-01-12 16:02:18 +0900

    [test] #14 장바구니 상품 생성 성공 실패 테스트 코드 작성

---
+++ backend/src/test/java/com/spicy/backend/order/application/CartItemServiceTests.java
@@ -0,0 +1,99 @@
+package com.spicy.backend.order.application;
+
+import com.spicy.backend.global.error.errorcode.GlobalErrorCode;
+import com.spicy.backend.global.error.exception.BusinessException;
+import com.spicy.backend.order.dao.ProductRepository;
+import com.spicy.backend.order.dao.cartitems.CartItemRepository;
+import com.spicy.backend.order.domain.CartItem;
+import com.spicy.backend.order.domain.Product;
+import com.spicy.backend.order.dto.request.CartItemCreateRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CartItemServiceTests {
+    @InjectMocks
+    private CartItemService cartItemService;
+
+    @Mock
+    private CartItemRepository cartItemRepository;
+    @Mock
+    private ProductRepository productRepository;
+
+    private Long userId;
+    private Long storeId;
+    private Long productId;
+    private Long quantity;
+    private Long cartId;
+
+    private CartItem cartItem;
+    private Product product;
+
+    CartItemCreateRequest cartItemCreateRequest;
+
+    @BeforeEach
+    void setUp() {
+        userId = 1L;
+        storeId = 10L;
+        productId = 100L;
+
+        quantity = 20L;
+
+        product = Product.builder()
+                .price(BigDecimal.valueOf(100.00))
+                .build();
+        ReflectionTestUtils.setField(product, "productId", productId);
+
+        cartItem = CartItem.builder()
+                .storeId(storeId)
+                .userId(userId)
+                .product(product)
+                .quantity(quantity)
+                .build();
+        ReflectionTestUtils.setField(cartItem, "id", cartId);
+
+        cartItemCreateRequest = new CartItemCreateRequest(
+                productId,
+                quantity
+        );
+    }
+
+    @Test
+    @DisplayName("장바구니 상품 생성 - 성공")
+    void addCartItem_Success() {
+        // given
+        given(productRepository.findById(productId)).willReturn(Optional.of(product));
+
+        // when
+        List<Long> response = cartItemService.addCartItem(userId, storeId, List.of(cartItemCreateRequest));
+
+        // then
+        assertEquals(cartId, response.get(0));
+    }
+    @Test
+    @DisplayName("장바구니 상품 생성 - 실패 - RESOURCE_NOT_FOUND")
+    void addCartItem_Failure_RESOURCE_NOT_FOUND() {
+        // given
+        given(productRepository.findById(productId)).willReturn(Optional.empty());
+
+        // when & then
+        BusinessException exception = assertThrows(BusinessException.class, () ->
+                cartItemService.addCartItem(userId, storeId, List.of(cartItemCreateRequest))
+        );
+        assertEquals(GlobalErrorCode.RESOURCE_NOT_FOUND, exception.getErrorCode());
+    }
+}
