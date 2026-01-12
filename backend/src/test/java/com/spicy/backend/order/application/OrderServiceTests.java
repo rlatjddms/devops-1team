94ee524b27e6c26b1be646092f5ee5a5ca0ea07e
chaewookim <chaewookim2877@gmail.com>
2026-01-12 12:23:43 +0900

    [test] #14 주문 취소 성공 실패 테스트 코드 작성

--- backend/src/test/java/com/spicy/backend/order/application/OrderServiceTests.java
+++ backend/src/test/java/com/spicy/backend/order/application/OrderServiceTests.java
@@ -8,6 +8,7 @@ import com.spicy.backend.order.domain.OrderItem;
 import com.spicy.backend.order.dto.request.OrderCreateRequest;
 import com.spicy.backend.order.dto.request.OrderItemRequest;
 import com.spicy.backend.order.dto.request.wrapper.OrderAndOrderItemRequest;
+import com.spicy.backend.order.dto.response.OrderCanceledResponse;
 import com.spicy.backend.order.dto.response.OrderCreateResponse;
 import com.spicy.backend.order.dto.response.OrderItemResponse;
 import com.spicy.backend.order.dto.response.OrderResponse;
@@ -25,7 +26,9 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -172,27 +175,58 @@ class OrderServiceTests {
     @DisplayName("주문 정보 상세 조회 - 성공")
     void getOrderDetails_Success() {
         // given
-        given(orderItemRepository.findAllByStoreIdAndOrderId(storeId, orderId)).willReturn(List.of(orderItem));
+        given(orderItemRepository.findAllByStoreIdAndOrderIdAndDeletedAtIsNullOrderByCreatedAtDesc(storeId, orderId)).willReturn(List.of(orderItem));
 
         // when
         List<OrderItemResponse> response = orderService.getOrderDetails(storeId, orderId);
 
         // then
-        assertEquals("productName",  response.get(0).productName());
+        assertEquals("productName", response.get(0).productName());
         assertEquals(quantity, response.get(0).quantity());
         assertEquals(unitPrice, response.get(0).unitPrice());
         assertEquals(totalPrice, response.get(0).totalPrice());
     }
+
     @Test
     @DisplayName("주문 정보 상세 조회 - 실패 - ORDER_ITEM_NOT_FOUND")
     void getOrderDetails_Failure_ORDER_ITEM_NOT_FOUND() {
         // given
-        given(orderItemRepository.findAllByStoreIdAndOrderId(storeId, orderId)).willReturn(List.of());
+        given(orderItemRepository.findAllByStoreIdAndOrderIdAndDeletedAtIsNullOrderByCreatedAtDesc(storeId, orderId)).willReturn(List.of());
 
         // when & then
         BusinessException exception = assertThrows(BusinessException.class, () ->
                 orderService.getOrderDetails(storeId, orderId));
         assertEquals("주문 상품이 존재하지 않거나 권한이 없습니다.", exception.getMessage());
         assertEquals(OrderErrorCode.ORDER_ITEM_NOT_FOUND, exception.getErrorCode());
+    }
+
+
+    @Test
+    @DisplayName("주문 취소 - 성공")
+    void cancelOrder_Success() {
+        // given
+        given(orderRepository.findByStoreIdAndIdAndDeletedAtIsNull(storeId, orderId)).willReturn(Optional.of(order));
+
+        given(orderItemRepository.findAllByStoreIdAndOrderIdAndDeletedAtIsNullOrderByCreatedAtDesc(storeId, orderId)).willReturn(List.of(orderItem));
+
+        // when
+        OrderCanceledResponse response = orderService.cancelOrder(storeId, orderId);
+
+        // then
+        assertEquals(Status.CANCELLED, response.order().status());
+        assertThat(orderItem)
+                .matches(item -> item.getStatus().equals(Status.CANCELLED));
+    }
+    @Test
+    @DisplayName("주문 실패 - ORDER_NOT_FOUND")
+    void cancelOrder_Failure_ORDER_NOT_FOUND() {
+        // given
+        given(orderRepository.findByStoreIdAndIdAndDeletedAtIsNull(storeId, orderId)).willReturn(Optional.empty());
+
+        // then & when
+        BusinessException exception = assertThrows(BusinessException.class, () ->
+                orderService.cancelOrder(storeId, orderId)
+        );
+        assertEquals(OrderErrorCode.ORDER_NOT_FOUND, exception.getErrorCode());
     }
 }
