e0a16f2ca5e5acce7460cccb658528e782af4eaa
yoocy0 <yoocy0328@gmail.com>
2026-01-12 17:35:03 +0900

    [feat] #21 getStock 메서드 구현

--- backend/src/main/java/com/spicy/backend/demandplan/adapter/ExternalDataFetcherImpl.java
+++ backend/src/main/java/com/spicy/backend/demandplan/adapter/ExternalDataFetcherImpl.java
@@ -2,21 +2,38 @@ package com.spicy.backend.demandplan.adapter;
 
 import com.spicy.backend.demandplan.controller.dto.StockResponseDto;
 import com.spicy.backend.demandplan.service.ExternalDataFetcher;
+import com.spicy.backend.inventory.application.InventoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ExternalDataFetcherImpl implements ExternalDataFetcher {
+
+    private final InventoryService inventoryService;
 
     @Override
     public StockResponseDto getStock(Long productId) {
-        // TODO: Inventory 서비스 코드 구현시 작성 예정
-        return null;
+
+        var inventoryData = inventoryService.searchProduct(productId);
+
+        if(inventoryData == null) {
+            throw new RuntimeException("상품을 조회할 수 없습니다.");
+        }
+
+        return new StockResponseDto(
+                inventoryData.productName(),
+                inventoryData.price(),
+                inventoryData.minimumQuantity(),
+                inventoryData.totalQuantity(),
+                inventoryData.products()
+        );
     }
 
     @Override
-    public List<Integer> getWeeklyOrderCount(Long productId) {
+    public List<Integer> getRecentOrderCount(Long productId) {
         // TODO: Settlement 서비스 코드 구현시 작성 예정
         return List.of();
     }
