1c46baa08522b73f6975a8c716b8004bff61dfe6
yoocy0 <yoocy0328@gmail.com>
2026-01-12 16:50:54 +0900

    [feat] #21 error 코드 íenum 파일 생성

---
+++ backend/src/main/java/com/spicy/backend/demandplan/error/DemandPlanErrorCode.java
@@ -0,0 +1,19 @@
+package com.spicy.backend.demandplan.error;
+
+import com.spicy.backend.global.error.errorcode.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum DemandPlanErrorCode implements BaseErrorCode {
+
+    // 판매 내역 조회 실패
+    FAILED_TO_FETCH_SALES(HttpStatus.NOT_FOUND, "해당 상품의 주문 내역을 찾을 수 없습니다.", "D001");
+
+
+    private final HttpStatus status;
+    private final String message;
+    private final String code;
+}
