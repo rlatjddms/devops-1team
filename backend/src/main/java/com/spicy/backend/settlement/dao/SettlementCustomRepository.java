322d6a091abfe78cec49451eff8e79f00f9dd091
yoocy0 <yoocy0328@gmail.com>
2026-01-14 20:15:15 +0900

    [feat] #53 주문 ìë역 조회 로직 추가 및 누락 필드 추가

--- backend/src/main/java/com/spicy/backend/settlement/dao/SettlementCustomRepository.java
+++ backend/src/main/java/com/spicy/backend/settlement/dao/SettlementCustomRepository.java
@@ -1,9 +1,12 @@
 package com.spicy.backend.settlement.dao;
 
 import com.spicy.backend.settlement.domain.Settlement;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SettlementCustomRepository {
     // Bulk Insert를 위한 커스텀 인터페이스
     void bulkInsertSettlements(List<Settlement> settlements);
+
 }
